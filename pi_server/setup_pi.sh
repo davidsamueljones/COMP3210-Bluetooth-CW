3953bd680309528b3caa94fd366e439998d34077
David Jones <davidsamueljones@icloud.com>
2019-05-11 20:25:54 +0100

    Added script for setting up Pi

---
+++ pi_server/setup_pi.sh
@@ -0,0 +1,11 @@
+ip=192.168.1.147
+
+scp python/packet_gen.py pi@$ip:packet_gen.py
+scp python/file_broadcast.py pi@$ip:file_broadcast.py
+scp python/advert_broadcast.py pi@$ip:advert_broadcast.py
+scp python/custom_lt.py pi@$ip:custom_lt.py
+scp python/encode_decode_test.py pi@$ip:encode_decode_test.py
+scp python/stop.py pi@$ip:stop.py
+scp python/setup.py pi@$ip:setup.py
+scp python/test_string.txt pi@$ip:test_string.txt
+scp python/km_small.png pi@$ip:km_small.png
