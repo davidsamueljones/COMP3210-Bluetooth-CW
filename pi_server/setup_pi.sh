0ef804f64a72a60545f15e58e77cf02b085bc4ea
David Jones <davidsamueljones@icloud.com>
2019-05-29 11:35:43 +0100

    Made setup script use a function
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- pi_server/setup_pi.sh
+++ pi_server/setup_pi.sh
@@ -1,13 +1,18 @@
-ip=192.168.43.156
 
-scp python/packet_gen.py pi@$ip:packet_gen.py
-scp python/file_broadcast.py pi@$ip:file_broadcast.py
-scp python/advert_broadcast.py pi@$ip:advert_broadcast.py
-scp python/btle_broadcast.py pi@$ip:btle_broadcast.py
-scp python/test_stream.py pi@$ip:test_stream.py
-scp python/custom_lt.py pi@$ip:custom_lt.py
-scp python/encode_decode_test.py pi@$ip:encode_decode_test.py
-scp python/stop.py pi@$ip:stop.py
-scp python/setup.py pi@$ip:setup.py
-scp python/test_string.txt pi@$ip:test_string.txt
-scp python/km_small.png pi@$ip:km_small.png
+setup(ip)
+{
+    scp python/packet_gen.py pi@$ip:packet_gen.py
+    scp python/file_broadcast.py pi@$ip:file_broadcast.py
+    scp python/advert_broadcast.py pi@$ip:advert_broadcast.py
+    scp python/btle_broadcast.py pi@$ip:btle_broadcast.py
+    scp python/test_stream.py pi@$ip:test_stream.py
+    scp python/custom_lt.py pi@$ip:custom_lt.py
+    scp python/encode_decode_test.py pi@$ip:encode_decode_test.py
+    scp python/stop.py pi@$ip:stop.py
+    scp python/setup.py pi@$ip:setup.py
+    scp python/test_string.txt pi@$ip:test_string.txt
+    scp python/km_small.png pi@$ip:km_small.png
+}
+
+setup(192.168.43.8)
+setup(192.168.43.156)
